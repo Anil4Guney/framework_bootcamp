1795b059f402ee97a9cd915232ed48316dba07d9
Anıl Güney <147307816+Anil4Guney@users.noreply.github.com>
2025-09-14 19:12:05 +0300

    a

--- 6-class-style-binding/app.js
+++ 6-class-style-binding/app.js
@@ -1,6 +1,15 @@
 const app = Vue.createApp({
     data() {
-        return {};
+        return {
+            showBorder : false,
+            boxClass : "border red",
+            redBG : false,
+        };
+    },
+    computed : {
+        boxClass(){
+            return { border : this.showBorder, red : this.redBG };
+        },
     },
 
 }).mount("#app")
---
+++ 8-loops/app.js
@@ -0,0 +1,8 @@
+const app = Vue.createApp({ 
+    data(){
+        return{
+            itemList : [ "Todo1", "Todo2", "Todo3" , "Todo4" ],
+        };
+    },
+
+}).mount("#app");
