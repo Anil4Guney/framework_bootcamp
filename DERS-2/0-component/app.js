c077e5ce652cabb722c52cf1ecb407a2e7df9b60
Anıl Güney <147307816+Anil4Guney@users.noreply.github.com>
2025-09-16 15:05:15 +0300

    a

---
+++ DERS-2/0-component/app.js
@@ -0,0 +1,24 @@
+const app = Vue.createApp({
+  data() {
+    return {
+//      counter : 0,   bunları sildik aşağıdakini yaptık habire tekrar etmemek için
+//      counter2 : 0,
+    };
+  },
+});
+
+app.component("counter-item",{
+  data(){
+    return{
+      counter: 0,
+    };
+  },
+  template : `
+      <div class="container-sm">
+        <h3 class="mb-2">{{ counter }}</h3>
+        <button @click="counter--" class="red sm">-</button>
+        <button @click="counter++" class="green sm">+</button>
+      </div>
+  `
+});
+app.mount("#app");
