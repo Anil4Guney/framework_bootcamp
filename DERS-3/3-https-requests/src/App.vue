d5e95db109189b2dc0d35cc940e46527ad52e28d
Anıl Güney <147307816+Anil4Guney@users.noreply.github.com>
2025-09-19 19:27:03 +0300

    a

---
+++ DERS-3/3-https-requests/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <h3>Alışveriş Listesi</h3>
+    <hr/>
+    <div class="my-2"> 
+        <input type="text" placeholder="ne alacaksın?"/>
+    </div>
+    <ul>
+      <li class="d-flex justify-content-between align-items-center">
+        <span>Eleman 1</span>
+        <button class="sm red">Sil</button>
+      </li>
+      <li class="d-flex justify-content-between align-items-center">
+        <span>Eleman 1</span>
+        <button class="sm red">Sil</button>
+      </li>
+      <li class="d-flex justify-content-between align-items-center">
+        <span>Eleman 1</span>
+        <button class="sm red">Sil</button>
+      </li>   
+    </ul>
+    <small class="mt-2 text-red d-flex justify-content-end align-items-center">5 adet alınıcak ürün vardır..</small>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  data() {
+    return{
+
+    };
+  }
+};
+</script>
+
