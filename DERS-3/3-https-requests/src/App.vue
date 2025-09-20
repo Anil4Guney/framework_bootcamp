17628326d60c6476dabe972cd0999d9e4b153bc3
Anıl Güney <147307816+Anil4Guney@users.noreply.github.com>
2025-09-20 17:27:11 +0300

    a

--- DERS-3/3-https-requests/src/App.vue
+++ DERS-3/3-https-requests/src/App.vue
@@ -3,9 +3,15 @@
     <h3>Alışveriş Listesi</h3>
     <hr/>
     <div class="my-2"> 
-        <input type="text" placeholder="ne alacaksın?"/>
+        <input type="text" placeholder="ne alacaksın?" @keydown.enter="onSave"/>
     </div>
-    <ul>
+    <ul v-if="itemsList.length > 0">
+      <li v-for="item in itemsList" :key="item.id" class="d-flex justify-content-between align-items-center">
+        <span>{{ item.title }}</span>
+        <button @click="onDelete(item)" class="sm red">Sil</button>
+      </li>
+
+      <!--
       <li class="d-flex justify-content-between align-items-center">
         <span>Eleman 1</span>
         <button class="sm red">Sil</button>
@@ -14,23 +20,56 @@
         <span>Eleman 1</span>
         <button class="sm red">Sil</button>
       </li>
-      <li class="d-flex justify-content-between align-items-center">
-        <span>Eleman 1</span>
-        <button class="sm red">Sil</button>
-      </li>   
+      -->
     </ul>
-    <small class="mt-2 text-red d-flex justify-content-end align-items-center">5 adet alınıcak ürün vardır..</small>
+    <div v-else class="bg-blue  text-white">
+      Herhangi bir ürün yoktur...
+    </div>
+    <small class="mt-2 text-red d-flex justify-content-end align-items-center">{{ itemCount }} adet alınıcak ürün vardır..</small>
   </div>
 </template>
 
 <script>
 
-
+import axios from "axios";
 export default {
   data() {
     return{
-
+      itemsList : []
     };
+  },
+  mounted(){
+    axios.get("http://localhost:3000/items").then(items_response => {
+      console.log('items_response :>>', items_response);
+      this.itemsList = items_response.data || [];
+      console.log('this.itemsList :>>', this.itemsList);
+    })
+  },
+  methods: {
+    onSave (event) {
+      const saveObject = {
+        title : event.target.value,
+        created_at : new Date(),
+        completed: false
+      };
+      axios.post("http://localhost:3000/items", saveObject).then(save_response => {
+        console.log(save_response);
+        this.itemsList.push(save_response.data);
+        event.target.value = '';
+        event.target.focus();  
+      });
+    },
+    onDelete(item){
+      axios.delete(`http://localhost:3000/items/${item.id}`).then(delete_response =>{
+        console.log(delete_response); 
+        this.itemsList = this.itemsList.filter(i => i.id != item.id);
+      });
+    }
+  },
+  computed : {
+    itemCount(){
+      return this.itemsList.length || 0;
+    }
   }
 };
 </script>
