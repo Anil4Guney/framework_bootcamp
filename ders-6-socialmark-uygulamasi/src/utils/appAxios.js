1bdada1bd0d6714afca9e2cf4ee3c159520bb181
Anıl Güney <147307816+Anil4Guney@users.noreply.github.com>
2025-09-27 02:19:00 +0300

    a

--- ders-6-socialmark-uygulamasi/src/utils/appAxios.js
+++ ders-6-socialmark-uygulamasi/src/utils/appAxios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const appAxios = axios.create({
-  baseURL: "http://localhost:3000"
+  baseURL: "http://localhost:3000", // burası JSON-server portu ile aynı olmalı
 });
 
 export default appAxios;
