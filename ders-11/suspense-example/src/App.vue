f28ebe467cb304f9f8fff1944e5b82a7972461f1
Anıl Güney <147307816+Anil4Guney@users.noreply.github.com>
2025-10-04 13:39:48 +0300

    a

---
+++ ders-11/suspense-example/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Todos from "./components/Todos.vue";
+import Users from "./components/Users.vue";
+
+
+</script>
+
+<template>
+  <Todos/>
+  <Users/>
+</template>
---
+++ ders-11/suspense-example/src/components/Todos.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue';
+const todoList = ref([]);
+  fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json => (todoList.value = json));
+
+
+</script>
+
+<template>
+    <h3>Todos</h3>
+    <ul>
+        <li v-for="todo in todoList" :key="todo.id">
+            {{ todo.title }}
+        </li>
+    </ul>
+</template>
